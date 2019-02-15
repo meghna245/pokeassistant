88b6ee4e98e90fe1619724142ac3b5e53f4f59bb
Glitch (hello-express) <none>
2019-02-15 08:26:05 +0000

    😅🍦 Checkpoint ./commands/help.js:2622129/1577 ./watch.json:2622129/2

--- commands/help.js
+++ commands/help.js
@@ -7,16 +7,33 @@ module.exports.run = async (client, message, args) => {
       .setColor(0xFF4500)
       .addField("🔢 Legend", "`<arg>` Compulsory argument\n`[arg]` Optional argument");
   
-    let countersArr = [],
-        utilityArr = [],
-        categoryArr = [],
-        otherArr = [];
+  let generalArr = [],
+      debugArr = [];
+      //otherArr = [];
         
-    bot.cmdhelp.filter(cmd => cmd.category === 'Counters').forEach((cmd) => {countersArr.push(cmd.displName)});
-    bot.cmdhelp.filter(cmd => cmd.category === 'Utility').forEach((cmd) => {utilityArr.push(cmd.displName)});
-    bot.cmdhelp.filter(cmd => cmd.category === 'Category').forEach((cmd) => {categoryArr.push(cmd.displName)});
-    bot.cmdhelp.filter(cmd => cmd.category === 'Other').forEach((cmd) => {otherArr.push(cmd.displName)});
-  };
+  client.cmdhelp.filter(cmd => cmd.category === "General").forEach((cmd) => {generalArr.push(cmd.name)});
+  client.cmdhelp.filter(cmd => cmd.category === "Debug").forEach((cmd) => {debugArr.push(cmd.name)});
+  //client.cmdhelp.filter(cmd => cmd.category === 'Other').forEach((cmd) => {otherArr.push(cmd.name)});
+  
+  embed
+    .addField("General", generalArr.map(g => g).join('\n'), true);
+  
+  if (!args[0]) {
+    message.channel.send(embed);
+  } else {
+    let cmd = client.cmdhelp.filter(cmd => cmd.name === args[0]).first();
+    if (!cmd) {
+      embed.setTitle('That command could not be found.');
+      message.channel.send(embed);
+    } else {
+      let cmdEmbed = new Discord.RichEmbed()
+        .setTitle(cmd.displName)
+        .setDescription(`${cmd.desc}\n\n**Usage:**\n\`\`\`${cmd.usage}\`\`\``)
+        .setColor('#73C3AB')
+      message.channel.send(cmdEmbed)
+    }
+  }
+};
 
 exports.help = {
   name: "help",
